6bdc8bc9f67f76181113a8a6fbc9ff0cd4216c4c
sven-gerlach <svengerlach@me.com>
2021-03-04 15:56:14 -0500

    Rename all remaining example modules

--- app/routes/workout_routes.js
+++ app/routes/workout_routes.js
@@ -4,7 +4,7 @@ const express = require('express')
 const passport = require('passport')
 
 // pull in Mongoose model for examples
-const Example = require('../models/example')
+const Example = require('../models/workout')
 
 // this is a collection of methods that help us detect situations when we need
 // to throw a custom error
