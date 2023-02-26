64669df9b3846e48ad09bbfc0852c1d5fe3be9a1
sven-gerlach <svengerlach@protonmail.com>
2023-02-26 15:31:25 -0500

    Amend server.js

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const { startApp } = require('./app');
 
-const app = startApp();
-
 // define port for API to run on
 const port = process.env.PORT || 3001
 
-// run API on designated port
-app.listen(port, () => {
-  console.log('listening on port ' + port)
-})
+async function listen() {
+  const app = await startApp();
+
+  // run API on designated port
+  app.listen(port, () => {
+    console.log('listening on port ' + port)
+  })
+}
+
+listen();
