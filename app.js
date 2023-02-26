6dcbca73b1c73643688e572d5a0e148ffc122f5a
sven-gerlach <svengerlach@protonmail.com>
2023-02-26 02:47:45 -0500

    Ensure app is cached

---
+++ app.js
@@ -0,0 +1,74 @@
+// require necessary NPM packages
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+// require route files
+const userRoutes = require('./app/routes/user_routes')
+const workoutRoutes = require('./app/routes/workout_routes')
+const healthRoutes = require('./app/routes/health_routes')
+
+// require middleware
+const errorHandler = require('./lib/error_handler')
+const requestLogger = require('./lib/request_logger')
+
+// require database configuration logic
+// `db` will be the actual Mongo URI as a string
+const mongoURI = require('./config/db')
+
+// require configured passport authentication middleware
+const auth = require('./lib/auth')
+
+// establish database connection
+// use new version of URL parser
+// use createIndex instead of deprecated ensureIndex
+async function startApp() {
+  await mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+  });
+
+  // instantiate express application object
+  const app = express()
+
+  // set CORS headers on response from this API using the `cors` NPM package
+  // `CLIENT_ORIGIN` is an environment variable that will be set on Heroku
+  app.use(cors({ origin: process.env.CLIENT_ORIGIN }))
+
+  // register passport authentication middleware
+  app.use(auth)
+
+  // add `express.json` middleware which will parse JSON requests into
+  // JS objects before they reach the route files.
+  // The method `.use` sets up middleware for the Express application
+  app.use(express.json())
+  app.set('json spaces', 2)
+
+  // this parses requests sent by `$.ajax`, which use a different content type
+  app.use(express.urlencoded({ extended: true }))
+
+  // log each request as it comes in for debugging
+  app.use(requestLogger)
+
+  app.get('/', (req, res) => {
+    res.send('Server is listening for requests')
+  })
+
+  // register route files
+  app.use(workoutRoutes)
+  app.use(userRoutes)
+  app.use(healthRoutes)
+
+  // register error handling middleware
+  // note that this comes after the route middlewares, because it needs to be
+  // passed any error messages from them
+  app.use(errorHandler)
+
+  return app;
+}
+
+// needed for testing and to initiate serverless deployment to AWS Lambda
+module.exports = {
+  startApp,
+}
--- serverless.js
+++ serverless.js
@@ -1,4 +1,19 @@
-const app = require('./server');
-const serverless = require('serverless-http');
+require('source-map-support/register')
+const { startApp } = require('./app');
+const serverlessExpress = require('@vendia/serverless-express');
 
-module.exports.handler = serverless(app);
+let serverlessExpressInstance
+
+async function setup(event, context, callback) {
+  const app = await startApp();
+
+  serverlessExpressInstance =  serverlessExpress({ app });
+  return serverlessExpressInstance(event, context, callback);
+}
+
+async function handler (event, context, callback) {
+  if (serverlessExpressInstance) return serverlessExpressInstance(event, context, callback);
+  return setup(event, context, callback);
+}
+
+exports.handler = handler
